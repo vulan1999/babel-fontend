0aaeb7dcefbefc432486aa8229b8051832fed90e
Vu Lan Nguyen <vulan1999@gmail.com>
2025-06-14 11:23:12 +0700

    call api successfully

--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -1,20 +1,43 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { Table, TableBody, TableBodyRow,TableBodyCell,TableHead, TableHeadCell } from 'flowbite-svelte'
 
+	let books = []
 	onMount(async() => {
 		try {
-			const response = await fetch('/books')
+			const response = await fetch('/api/books')
 			if(!response.ok){
 				throw new Error(`HTTP Error`);
 			}
-			books = await response.json();
-			console.log(books);
-		} catch (e) {
+			let res = await response.json();
+			console.log(res);
+			books = res.data;
+		} catch (err) {
 			console.log("Error");
+			console.log(err)
 		} finally {
 			console.log("OK")
 		}
 	})
 </script>
 
-<h1>Hello Books</h1>
+<div class="p-4">
+	<Table>
+		<TableHead>
+			<TableHeadCell>Title</TableHeadCell>
+			<TableHeadCell>Author</TableHeadCell>
+			<TableHeadCell>Original Title</TableHeadCell>
+			<TableHeadCell>Language</TableHeadCell>
+		</TableHead>
+		<TableBody>
+			{#each books as book (book.id)}
+				<TableBodyRow>
+					<TableBodyCell>{book.title}</TableBodyCell>
+					<TableBodyCell>{book.author.author_name}</TableBodyCell>
+					<TableBodyCell>{book.original_book.original_book_title}</TableBodyCell>
+					<TableBodyCell>{book.language.language_description}</TableBodyCell>
+				</TableBodyRow>
+			{/each}
+		</TableBody>
+	</Table>
+</div>
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		proxy: {
+			'/api': { // Any request to your Svelte dev server starting with /api
+				target: 'http://localhost:8080', // Will be forwarded to your Go backend
+				changeOrigin: true, // Needed for virtual hosted sites
+			},
+		},
+	},
 });
